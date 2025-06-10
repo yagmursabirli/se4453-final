253bf0cfe68bbb26c0ed531b7a0f17afb42f7815
yagmursabirli <yagmursabirli35@gmail.com>
2025-06-10 17:09:37 +0300

    first commit

---
+++ final-web-app/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import jakarta.servlet.*;
+import java.io.*;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from Dockerized Java App!♥</h1>");
+    }
+}
